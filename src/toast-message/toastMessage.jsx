324b5f296b857f45505b9f2b247ff7c273ef99b4
Shrihari Eknathe <shriharieknathe999@gmail.com>
2025-04-05 14:54:31 +0530

    animation add for both left and right

--- src/toast-message/toastMessage.jsx
+++ src/toast-message/toastMessage.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 const ToastMessage = () => {
   const [toastList, setToastList] = useState([]);
+  const [shiftButton, setShiftButton] = useState("left");
 
   const timeRef = useRef({});
   console.log(timeRef);
@@ -27,35 +28,68 @@ const ToastMessage = () => {
 
   return (
     <div className="toast_component">
-      <div className={`toast_container `}>
+      <div
+        className={`toast_container`}
+        style={{
+          [shiftButton]: "10px",
+        }}
+      >
         {toastList.map((item) => {
           return (
-            <div key={item.id} className={`toast ${item.type}`}>
+            <div
+              key={item.id}
+              className={`toast ${item.type}`}
+              style={{
+                animation:
+                  shiftButton === "right" ? "slide-right 1s" : "slide-left 1s",
+              }}
+            >
               {item.message}
               <span onClick={() => onCloseClick(item.id)}>X</span>
             </div>
           );
         })}
       </div>
-      <div className="button_container">
-        <button
-          className="btn"
-          onClick={() => onButtonClick("Success", "success")}
-        >
-          Success
-        </button>
-        <button
-          className="btn"
-          onClick={() => onButtonClick("Warning", "warning")}
-        >
-          Warning
-        </button>
-        <button className="btn" onClick={() => onButtonClick("Error", "error")}>
-          Error
-        </button>
-        <button className="btn" onClick={() => onButtonClick("Info", "info")}>
-          Info
-        </button>
+      <div className="box">
+        <div className="button_container">
+          <button
+            className="btn"
+            onClick={() => onButtonClick("Success", "success")}
+          >
+            Success
+          </button>
+          <button
+            className="btn"
+            onClick={() => onButtonClick("Warning", "warning")}
+          >
+            Warning
+          </button>
+          <button
+            className="btn"
+            onClick={() => onButtonClick("Error", "error")}
+          >
+            Error
+          </button>
+          <button className="btn" onClick={() => onButtonClick("Info", "info")}>
+            Info
+          </button>
+        </div>
+        <div className="shift">
+          <button
+            disabled={shiftButton === "left"}
+            className="btn"
+            onClick={() => setShiftButton("left")}
+          >
+            left
+          </button>
+          <button
+            disabled={shiftButton === "right"}
+            className="btn"
+            onClick={() => setShiftButton("right")}
+          >
+            right
+          </button>
+        </div>
       </div>
     </div>
   );
